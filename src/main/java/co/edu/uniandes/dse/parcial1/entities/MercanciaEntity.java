fb9b7dda6c76ccdaf9be04f64770decc6b372525
k-garces <kellygarce@gmail.com>
2025-09-15 12:42:30 -0500

    Update MercanciaEntity.java

--- src/main/java/co/edu/uniandes/dse/parcial1/entities/MercanciaEntity.java
+++ src/main/java/co/edu/uniandes/dse/parcial1/entities/MercanciaEntity.java
@@ -6,9 +6,7 @@ import lombok.Data;
 @Data
 @Entity
 public class MercanciaEntity extends BaseEntity{
-    private String nombre;
-    private String descripcion;
-    private Double peso;
+   
     
 
 }
